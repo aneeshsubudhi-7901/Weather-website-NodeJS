6f51129c25d6ef65abd1a939733a0fbc6c017a6d
aneesh <aneeshs7bd@gmail.com>
2021-05-08 08:03:56 +0530

    Change the weather text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,7 @@ const forecast = (lat, long, callback) => {
     } else if (body.error) {
       callback("Unable to find location!", undefined);
     } else {
-      const temp = `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`;
+      const temp = `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out. The humidity is ${body.current.humidity}%.`;
       callback(undefined, temp);
     }
   });
